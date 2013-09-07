90376c16bf683d22e329ee48f26a70fd22341a0a
GonJay <assyer@gmail.com>
2013-09-07 12:43:42 +0800

    first commit

---
+++ democoffee/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Democoffee::Application.config.secret_key_base = '69d4ab47500bc7abf97b0cecab6ea4fb6acf2b8a484734f26333fb980dd6d16c13ac8f4f558e46873546e5caf838ab3d03987e91eeb9cafd45d64f5d9fa2d2ba'
